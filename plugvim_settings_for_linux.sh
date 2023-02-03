a4da18260ec58bfdbf610c5cb9c16b0c23d7206b
take1014 <y.takehara1014@gmail.com>
2023-02-03 10:41:31 +0900

    update shell

--- plugvim_settings_for_linux.sh
+++ plugvim_settings_for_linux.sh
@@ -1,2 +1,5 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./linux/.vimrc ~/
+vim -c PlugInstall -c q -c q!
+mkdir ~/.vim/colors
+cp ~/.vim/plugged/jellybeans.vim/colors/jellybeans.vim ~/.vim/colors/
--- plugvim_settings_for_win.ps1
+++ plugvim_settings_for_win.ps1
@@ -1,2 +1,5 @@
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |` ni $HOME/vimfiles/autoload/plug.vim -Force
 Copy-Item -Path ./windows/.vimrc -Destination $HOME/ -Force
+vim -c PlugInstall -c q -c q!
+New-Item $HOME/vimfiles/colors -ItemType Directory
+Copy-Item -Path $HOME/.vim/plugged/jellybeans.vim/colors/jellybeans.vom -Destination $HOME/vimfiles/colors/
