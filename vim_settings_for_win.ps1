a58850481be569496801399472dff5ce8086205c
take1014 <y.takehara1014@gmail.com>
2023-05-13 10:24:15 +0900

    update win

--- vim_settings_for_win.ps1
+++ vim_settings_for_win.ps1
@@ -2,4 +2,4 @@ iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
 Copy-Item -Path ./windows/.vimrc -Destination $HOME/ -Force
 vim -c PlugInstall -c q -c q!
 New-Item $HOME/vimfiles/colors -ItemType Directory
-Copy-Item -Path $HOME/.vim/plugged/jellybeans.vim/colors/jellybeans.vom -Destination $HOME/vimfiles/colors/
+Copy-Item -Path $HOME/.vim/plugged/jellybeans.vim/colors/jellybeans.vim -Destination $HOME/vimfiles/colors/
