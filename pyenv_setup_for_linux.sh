fe315ab2525b10e2c4f7e1a8d995720c0cfea3f0
take1014 <y.takehara1014@gmail.com>
2023-10-22 08:45:03 +0900

    update pyenv setup

--- pyenv_setup_for_linux.sh
+++ pyenv_setup_for_linux.sh
@@ -1,15 +1,22 @@
 # update & upgrade
 sudo apt-get update
 sudo apt-get upgrade
-# install poetry
-sudo apt-get install python3-poetry
 # clone pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 # add setting to .bashrc
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+# install pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 # install dependencies
-sudo apt install build-essential libssl-dev zlib1g-dev \
+sudo apt-get install build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libsasl2-dev python3-dev libldap2-dev
+# install poetry
+sudo apt-get install python3-poetry
+# set poetry's workdir
+poetry config virtualenvs.in-project true
+# reboot bash
+source ~/.bashrc
