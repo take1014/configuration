5c03feac7325f072bc75e6868057462bd983d230
take1014 <y.takehara1014@gmail.com>
2023-10-28 18:57:00 +0900

    update for linux

--- vim_settings_for_linux.sh
+++ vim_settings_for_linux.sh
@@ -1,3 +1,4 @@
+sudo apt-get install curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./linux/.vimrc ~/
 vim -c PlugInstall -c q -c q!
