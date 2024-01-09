ba1bf772c2a506db65824f9d32680354c7aaca76
take1014 <y.takehara1014@gmail.com>
2024-01-10 01:39:18 +0900

    oops

--- vim_settings_for_linux.sh
+++ vim_settings_for_linux.sh
@@ -1,12 +1,24 @@
 # vim
 # build vim
 sudo apt-get update
-sudo apt-get install libncurses5-dev libx11-dev libxtst-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
+sudo apt-get install libncurses5-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
 git clone https://github.com/vim/vim.git
 cd vim
+
+# Deactivate Conda environment if activated!!!!!!
 # you must configure --with-python3-config-dir for your python3 enviroment.
-# if you use conda, you must deactivate conda enviroment.
-./configure --with-features=huge --enable-multibyte --disable-rubyinterp --enable-python3interp --with-python3-config-dir=/lib/python3.10/config-3.10-x86_64-linux-gnu --disable-perlinterp --disable-luainterp --enable-fail-if-missing --prefix=/usr/local --disable-gui --with-x
+PYTHON3_CONFIG_DIR=/lib/python3.10/config-3.10-x86_64-linux-gnu
+./configure --with-features=huge \
+            --enable-multibyte \
+            --disable-rubyinterp \
+            --enable-python3interp \
+            --with-python3-config-dir=$PYTHON3_CONFIG_DIR \
+            --disable-perlinterp \
+            --disable-luainterp \
+            --enable-fail-if-missing \
+            --prefix=/usr/local \
+            --disable-gui \
+            --with-x
 make -j4
 sudo make install
 cd ..
