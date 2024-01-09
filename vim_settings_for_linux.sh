4efa923fd1463d278aa4321e775bfd86636ec97a
take1014 <y.takehara1014@gmail.com>
2024-01-10 00:47:12 +0900

    update vim settings

--- vim_settings_for_linux.sh
+++ vim_settings_for_linux.sh
@@ -1,4 +1,17 @@
 # vim
+# build vim
+sudo apt-get update
+sudo apt-get install libncurses5-dev libx11-dev libxtst-dev
+git clone https://github.com/vim/vim.git
+cd vim
+# you must configure --with-python3-config-dir for your python3 enviroment.
+# if you use conda, you must deactivate conda enviroment.
+./configure --with-features=huge --enable-multibyte --disable-rubyinterp --enable-python3interp --with-python3-config-dir=/lib/python3.10/config-3.10-x86_64-linux-gnu --disable-perlinterp --disable-luainterp --enable-fail-if-missing --prefix=/usr/local --disable-gui --with-x
+make -j4
+sudo make install
+cd ..
+
+# install plugins
 sudo apt-get install curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./linux/.vimrc ~/
