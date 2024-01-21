ca50c4b07bc18534749150bb5749d158ed3c51b2
take1014 <y.takehara1014@gmail.com>
2024-01-21 12:39:26 +0900

    add install shell

---
+++ vscode/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+extensions=(
+    "christian-kohler.npm-intellisense"
+    "christian-kohler.path-intellisense"
+    "eamodio.gitlens"
+    "GitHub.vscode-pull-request-github"
+    "golang.go"
+    "janisdd.vscode-edit-csv"
+    "jeff-hykin.better-cpp-syntax"
+    "mosapride.zenkaku"
+    "ms-python.isort"
+    "ms-python.python"
+    "ms-python.vscode-pylance"
+    "ms-toolsai.jupyter"
+    "ms-toolsai.jupyter-keymap"
+    "ms-toolsai.jupyter-renderers"
+    "ms-toolsai.vscode-jupyter-cell-tags"
+    "ms-toolsai.vscode-jupyter-slideshow"
+    "ms-vscode.cmake-tools"
+    "ms-vscode.cpptools"
+    "ms-vscode.cpptools-extension-pack"
+    "ms-vscode.cpptools-themes"
+    "PKief.material-icon-theme"
+    "shardulm94.trailing-spaces"
+    "siwatts.jellybeans-vim-theme"
+    "tomoki1207.pdf"
+    "twxs.cmake"
+    "VisualStudioExptTeam.intellicode-api-usage-examples"
+    "VisualStudioExptTeam.vscodeintellicode"
+    "vscodevim.vim"
+)
+
+for extension in "${extensions[@]}"
+do
+    code --install-extension "$extension"
+done
