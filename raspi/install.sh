da8b082a377118a435eaa1109b2e6d571221f0fe
take1014 <y.takehara1014@gmail.com>
2024-05-18 17:07:51 +0900

    update installer

--- raspi/install.sh
+++ raspi/install.sh
@@ -12,3 +12,10 @@ vim -c PlugInstall -c q -c q!
 mkdir ~/.vim/colors
 cp ~/.vim/plugged/jellybeans.vim/colors/jellybeans.vim ~/.vim/colors/
 cp ../linux/.tmux.conf ~/
+# edit bashrc
+echo "alias ls='ls --color'" >> ~/.bashrc
+echo "alias ll='ls -a'" >> ~/.bashrc
+echo "export CLICOLOR=1" >> ~/.bashrc
+echo "export EDITOR='vim'" >> ~/.bashrc
+echo "export PS1='\\u@\\h:\\[\\e[01;32m\\]\\w\\[\\e[0m\\]\\$ '" >> ~/.bashrc
+source ~/.bashrc
