6893407a5b0c706a9927e91d95d3f45584a210d2
take1014 <y.takehara1014@gmail.com>
2024-05-22 13:47:51 +0900

    delete space

--- raspi/install.sh
+++ raspi/install.sh
@@ -3,7 +3,7 @@ sudo apt-get full-upgrade
 sudo apt-get install -y vim tmux python3-dev python3-venv libncurses5-dev libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
 # change symbolic link for vi
 sudo rm /usr/bin/vi
-sudo ln -s /usr/bin/vim /usr/bin/vi 
+sudo ln -s /usr/bin/vim /usr/bin/vi
 # install plugins
 sudo apt-get install -y curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
