5a98b9f5b2dcdca2219bdbcfac13082dc7e35fb8
take1014 <y.takehara1014@gmail.com>
2024-12-08 23:09:00 +0900

    update installer

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y software-properties-common apt-transport-https wget
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
-rm microsoft.gpg
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-sudo apt update
-sudo apt install -y code
 
 extensions=(
     "christian-kohler.npm-intellisense"
